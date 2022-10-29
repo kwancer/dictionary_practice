3b3556074128238f4792a785f8f053ced7e5d062
Krzysztof Wancerski <79153882+kwancer@users.noreply.github.com>
2022-10-29 11:11:48 +0100

    finished display_inventory function

--- inventory.py
+++ inventory.py
@@ -1,7 +1,8 @@
 """file with the inventory function"""
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
-def displayInventory(inventory):
+def display_inventory(inventory):
+    """function which displays the user's inventory as a friendly list"""
     print("Inventory:")
     item_total = 0
     for k, v in inventory.items():
@@ -9,4 +10,4 @@ def displayInventory(inventory):
         item_total+=1
     print("Total number of items: " + str(item_total))
 
-displayInventory(stuff)
+display_inventory(stuff)
