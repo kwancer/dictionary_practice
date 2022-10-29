95522a3ab16bfe59708d293e2cc774450484cadb
Krzysztof Wancerski <79153882+kwancer@users.noreply.github.com>
2022-10-29 11:10:06 +0100

    added inventory.py file

---
+++ inventory.py
@@ -0,0 +1,12 @@
+"""file with the inventory function"""
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(str(k)+ " "+ str(v))
+        item_total+=1
+    print("Total number of items: " + str(item_total))
+
+displayInventory(stuff)
